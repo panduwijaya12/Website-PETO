f35c77c61020d3f168e4b5438d6ccbe0bec99f21
panduwijaya12 <youremail@example.com>
2024-12-26 21:26:01 +0800

    test

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,13 @@
+// src/components/Footer.js
 import React from 'react';
+import './Footer.css';
 
 function Footer() {
   return (
-    <footer>
-      <div className="footer-content">
-        <p>Follow us on Instagram: <a href="https://www.instagram.com/peto_event/" target="_blank" rel="noopener noreferrer">@peto_event</a></p>
+    <footer className="footer">
+      <p>&copy; 2024 Peto Organizer. Follow us on:</p>
+      <div className="social-media">
+        <a href="https://instagram.comhttps://www.instagram.com/petoevent/?utm_source=ig_web_button_share_sheet" target="_blank" rel="noopener noreferrer">Instagram</a> | 
       </div>
     </footer>
   );
