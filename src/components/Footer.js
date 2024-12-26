b0006d4d01be414722e134314461b46bea468a75
panduwijaya12 <youremail@example.com>
2024-12-26 21:29:06 +0800

    test

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
     <footer className="footer">
       <p>&copy; 2024 Peto Organizer. Follow us on:</p>
       <div className="social-media">
-        <a href="https://instagram.comhttps://www.instagram.com/petoevent/?utm_source=ig_web_button_share_sheet" target="_blank" rel="noopener noreferrer">Instagram</a> | 
+        <a href="https://instagram.comhttps://www.instagram.com/petoevent/?utm_source=ig_web_button_share_sheet" target="_blank" rel="noopener noreferrer">Instagram</a>
       </div>
     </footer>
   );
